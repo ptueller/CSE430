99e615b24ce779baa6724e6caa2d7bc23a99f2dc
Peter Tueller <ptueller7@gmail.com>
2015-09-19 17:17:54 -0700

    Filling in functions
    Honestly the way the professor is asking us to write the code is not
    the greatest way to implement this. It removes the ability to make
    multiple queues by requiring a queue global data structure. You
    wouldn’t need that global data structure if the DelQueue function
    returned a pointer to the new head node instead of the item that was
    deleted. Also we need to check boundary conditions for an almost empty
    queue, specifically on delqueue.

--- Project 1/q.h
+++ Project 1/q.h
@@ -5,6 +5,10 @@ typedef struct qnode {
 	int payload;
 } qnode;
 
+typedef struct queue {
+	qnode *head;
+} queue;
+
 qnode* NewItem() {
 	return malloc(sizeof(qnode));
 }
@@ -14,15 +18,27 @@ void FreeItem(qnode* head) {
 }
 
 void InitQueue(qnode* head) {
-	
+	head->next = head;
+	head->prev = head;
+	queue->head = head;
 }
 
 void AddQueue(qnode* head, qnode* item) {
-	
+	qnode *iterator = head;
+	while(iterator->next != head) {
+		iterator = iterator->next;
+	}
+	item->next = head;
+	item->prev = iterator;
+	iterator->next = item;
+	head->prev = item;
 }
 
 qnode* DelQueue(qnode* head) {
-	
+	head->next->prev = head->prev;
+	head->prev->next = head->next;
+	queue->head = head->next;
+	return head;
 }
 
 void RotateQueue(qnode* head) {
