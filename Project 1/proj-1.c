0a51b0f2efe09e1fc13ac83c11370eafa7290e13
Peter Tueller <ptueller7@gmail.com>
2015-09-20 13:57:29 -0700

    Initial basic test

--- Project 1/proj-1.c
+++ Project 1/proj-1.c
@@ -1,5 +1,19 @@
 #include "q.h"
+#include <stdlib.h>
 
 int main() {
-
+	queue *testQ1 = malloc(sizeof queue);
+	testQ1->head = NewItem();
+	testQ1->head->payload = 1;
+	InitQueue(testQ1->head);
+	qnode *tempNode = NewItem();
+	tempNode->payload = 2;
+	AddQueue(testQ1->head,tempNode);
+	printf("Payload 1 = %d. Payload 2 = %d\n",testQ1->head->payload,testQ1->head->next->payload);
+	DelQueue(testQ1->head);
+	printf("Payload 1 = %d. Payload 2 = %d\n",testQ1->head->payload,testQ1->head->next->payload);
+	DelQueue(testQ1->head);
+	if(testQ1->head != NULL) {
+		printf("Payload 1 = %d. Payload 2 = %d\n",testQ1->head->payload,testQ1->head->next->payload);
+	}
 }
